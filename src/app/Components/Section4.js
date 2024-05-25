d0568454118dca3503ce3b5247f23afcf5a0c7f9
zaidtariq-t <zaid.t@turing.com>
2024-05-26 00:00:02 +0500

    added desktop and mobile images

--- src/app/Components/Section4.js
+++ src/app/Components/Section4.js
@@ -1,34 +1,52 @@
 const Section4 = () => {
   return (
     <section className="section-4">
-      <div className="top">
-        <h2>
-          <div data-aos="fade-up">Turn data into real</div>
-          <div data-aos="fade-up" data-aos-delay="100">
-            actions and ideas.
-          </div>
-        </h2>
+      <div className="flex">
+        <div className="top">
+          <h2>
+            <div data-aos="fade-up">Turn data into real</div>
+            <div data-aos="fade-up" data-aos-delay="100">
+              actions and ideas.
+            </div>
+          </h2>
 
-        <ul>
-          <li data-aos="fade-up">
-            <span>Instant Insights</span>
-            <button className="icon">
-              <i className="fa fa-plus"></i>
-            </button>
-          </li>
-          <li data-aos="fade-up">
-            <span>AI Technology</span>
-            <button className="icon">
-              <i className="fa fa-plus"></i>
-            </button>
-          </li>
-          <li data-aos="fade-up">
-            <span>Easy Integration</span>
-            <button className="icon">
-              <i className="fa fa-plus"></i>
-            </button>
-          </li>
-        </ul>
+          <ul>
+            <li data-aos="fade-up">
+              <span>Instant Insights</span>
+              <button className="icon">
+                <i className="fa fa-plus"></i>
+              </button>
+            </li>
+            <li data-aos="fade-up">
+              <span>AI Technology</span>
+              <button className="icon">
+                <i className="fa fa-plus"></i>
+              </button>
+            </li>
+            <li data-aos="fade-up">
+              <span>Easy Integration</span>
+              <button className="icon">
+                <i className="fa fa-plus"></i>
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <img
+            className="ramos-desktop"
+            src="section-4-desktop.png"
+            alt="desktop"
+            data-aos="fade-up"
+          />
+
+          <img
+            className="ramos-mobile"
+            src="ramos-mobile.png"
+            alt="ramos-mobile"
+            data-aos="fade-up"
+          />
+        </div>
       </div>
 
       <div className="giant-ramos">Ramos</div>
